180531c779a85dc4bddc7f6c1ea5b730bb2899eb
Yuri Kim <yuri7kim@gmail.com>
2021-02-21 08:20:16 -0500

    onClick listeners for options

--- app/src/main/java/com/yuri/yuriflashcardapp/MainActivity.java
+++ app/src/main/java/com/yuri/yuriflashcardapp/MainActivity.java
@@ -23,9 +23,32 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener(){
             @Override
-                    public void onClick(View v){
+            public void onClick(View v){
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.option_one).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                findViewById(R.id.option_one).setBackgroundColor(getResources().getColor(R.color.red, null));
+                findViewById(R.id.option_two).setBackgroundColor(getResources().getColor(R.color.green, null));
+            }
+        });
+
+        findViewById(R.id.option_three).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                findViewById(R.id.option_three).setBackgroundColor(getResources().getColor(R.color.red, null));
+                findViewById(R.id.option_two).setBackgroundColor(getResources().getColor(R.color.green, null));
+            }
+        });
+
+        findViewById(R.id.option_two).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                findViewById(R.id.option_two).setBackgroundColor(getResources().getColor(R.color.green, null));
             }
         });
     }
