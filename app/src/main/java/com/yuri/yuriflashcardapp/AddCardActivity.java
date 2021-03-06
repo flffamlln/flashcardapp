3fec7ad59134c029cb2a152cded05af7f4d2327b
Yuri Kim <yuri7kim@gmail.com>
2021-03-06 17:12:45 -0500

    can change notecard

--- app/src/main/java/com/yuri/yuriflashcardapp/AddCardActivity.java
+++ app/src/main/java/com/yuri/yuriflashcardapp/AddCardActivity.java
@@ -2,7 +2,11 @@ package com.yuri.yuriflashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class AddCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(AddCardActivity.this, MainActivity.class);
+                AddCardActivity.this.startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.save).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent data = new Intent();
+                data.putExtra("question", ((EditText) findViewById(R.id.enterquestion)).getText().toString());
+                data.putExtra("answer", ((EditText) findViewById(R.id.enteranswer)).getText().toString());
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/yuri/yuriflashcardapp/MainActivity.java
+++ app/src/main/java/com/yuri/yuriflashcardapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,29 +15,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
             }
         });
 
-        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
             }
         });
 
-        findViewById(R.id.add).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                MainActivity.this.startActivity(intent);
+                MainActivity.this.startActivityForResult(intent, 100);
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String question = data.getExtras().getString("question"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String answer = data.getExtras().getString("answer");
+            ((TextView)findViewById(R.id.flashcard_question)).setText(question);
+            ((TextView)findViewById(R.id.flashcard_answer)).setText(answer);
+        }
     }
 }
