625cff26127bb80adf403ece0cc6bc91133a80f7
Yuri Kim <yuri7kim@gmail.com>
2021-03-06 19:51:29 -0500

    optional features

--- app/src/main/java/com/yuri/yuriflashcardapp/AddCardActivity.java
+++ app/src/main/java/com/yuri/yuriflashcardapp/AddCardActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -15,22 +17,28 @@ public class AddCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
 
-        findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(AddCardActivity.this, MainActivity.class);
                 AddCardActivity.this.startActivity(intent);
             }
         });
 
-        findViewById(R.id.save).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent data = new Intent();
-                data.putExtra("question", ((EditText) findViewById(R.id.enterquestion)).getText().toString());
-                data.putExtra("answer", ((EditText) findViewById(R.id.enteranswer)).getText().toString());
-                setResult(RESULT_OK, data);
-                finish();
+            public void onClick(View v) {
+                String question = ((EditText) findViewById(R.id.enterquestion)).getText().toString();
+                String answer = ((EditText) findViewById(R.id.enteranswer)).getText().toString();
+                if(question.length() == 0 && answer.length() == 0){
+                    Toast.makeText(getApplicationContext(), "Must Enter Question and Answer", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent data = new Intent();
+                    data.putExtra("question", question);
+                    data.putExtra("answer", answer);
+                    setResult(RESULT_OK, data);
+                    finish();
+                }
             }
         });
     }
